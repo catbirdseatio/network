f1efe7333b0800843bf8d6d9d9b79299387433cd
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-11-07 18:54:11 -0500

    test clean method on post model

--- tests/models/test_posts.py
+++ tests/models/test_posts.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 from tests.factories import PostFactory
 
@@ -9,7 +10,13 @@ class TestPost:
     def test_post_model(self, test_user, test_post):
         assert test_post.author.username == test_user.username
         assert len(test_post.body) == 127
-
+    
+    def test_post_model_clean_raises_validation_error(self, test_user):
+        with pytest.raises(ValidationError):
+            body = f"{'A'*250}"
+            post = PostFactory(body=body, author=test_user)
+            post.clean()
+        
     def test_post___str__full_length(self, test_post):
         assert f"{test_post.body[:25]}..." == str(test_post)
 
